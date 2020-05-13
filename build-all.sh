a329ab9fe7b94da2af5937a148444d8989912285
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2020-05-13 11:49:05 +0200

    Create Cmk/mainconfig.cmake to be used ini every subprojects BAD HACK: path to Cmk must be set in an environment variable with command line as well in CLion.

--- build-all.sh
+++ build-all.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-BUILD_DIR=build
+export BUILD_DIR=$(pwd)/build
+export INC_DIR=$(pwd)/Cmk
 [ -d ${BUILD_DIR} ] && rm -rf ${BUILD_DIR}
 mkdir ${BUILD_DIR}
 
-cmake -S . -B build || echo "cmake init failure"
+cmake -S . -B build || echo "cmake init failure" || exit 1
 cd ${BUILD_DIR}
-cmake --build . || echo "cmake build failure"
+cmake --build . || echo "cmake build failure" || exit 1
 
 # test stdin with pipes
 cat ../LICENSE | ./hexdump/hexdump.exe
+./hexdump/hexdump.exe --help
